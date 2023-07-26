b96a723cce39770034f5a90a83118b50861d0e80
osawa-koki <github@osawa.dev>
2023-07-26 21:31:59 +0900

    シーディング。

--- database/seeders/PrefecturesTableSeeder.php
+++ database/seeders/PrefecturesTableSeeder.php
@@ -12,6 +12,31 @@ class PrefecturesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $prefectures = [
+            [
+                "name" => "北海道",
+                "capital" => "札幌市",
+                "population" => 5_139_522,
+                "area" => 78_420,
+                "visited" => true,
+            ],
+            [
+                "name" => "青森県",
+                "capital" => "青森市",
+                "population" => 1_204_372,
+                "area" => 9_645,
+                "visited" => true,
+            ],
+            [
+                "name" => "岩手県",
+                "capital" => "盛岡市",
+                "population" => 1_180_512,
+                "area" => 15_275,
+                "visited" => false,
+            ]
+        ];
+        foreach ($prefectures as $prefecture) {
+            \App\Models\Prefecture::create($prefecture);
+        }
     }
 }
