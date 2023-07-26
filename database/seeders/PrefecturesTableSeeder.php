5d4dd6388845a05fb05dac70c412fc74f41d0ffb
osawa-koki <github@osawa.dev>
2023-07-26 21:32:35 +0900

    ./vendor/bin/pint

--- database/seeders/PrefecturesTableSeeder.php
+++ database/seeders/PrefecturesTableSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PrefecturesTableSeeder extends Seeder
@@ -14,26 +13,26 @@ class PrefecturesTableSeeder extends Seeder
     {
         $prefectures = [
             [
-                "name" => "北海道",
-                "capital" => "札幌市",
-                "population" => 5_139_522,
-                "area" => 78_420,
-                "visited" => true,
+                'name' => '北海道',
+                'capital' => '札幌市',
+                'population' => 5_139_522,
+                'area' => 78_420,
+                'visited' => true,
             ],
             [
-                "name" => "青森県",
-                "capital" => "青森市",
-                "population" => 1_204_372,
-                "area" => 9_645,
-                "visited" => true,
+                'name' => '青森県',
+                'capital' => '青森市',
+                'population' => 1_204_372,
+                'area' => 9_645,
+                'visited' => true,
             ],
             [
-                "name" => "岩手県",
-                "capital" => "盛岡市",
-                "population" => 1_180_512,
-                "area" => 15_275,
-                "visited" => false,
-            ]
+                'name' => '岩手県',
+                'capital' => '盛岡市',
+                'population' => 1_180_512,
+                'area' => 15_275,
+                'visited' => false,
+            ],
         ];
         foreach ($prefectures as $prefecture) {
             \App\Models\Prefecture::create($prefecture);
