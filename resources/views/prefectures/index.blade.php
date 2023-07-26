e581426f28e83a1a8080814c819f13c9bde31a1d
osawa-koki <github@osawa.dev>
2023-07-27 00:21:36 +0900

    フィルタリング画面。

--- resources/views/prefectures/index.blade.php
+++ resources/views/prefectures/index.blade.php
@@ -31,4 +31,73 @@
         @endforeach
     </table>
     <div><a href="/prefectures/create" class="btn btn-outline-secondary">新規作成</a></div>
+    <hr class="my-5" />
+    <form action="/prefectures" method="get" class="mt-5">
+        <table class="table text-left">
+            <tbody>
+                <tr>
+                    <th>都道府県名で検索</th>
+                    <td><input type="text" name="name" class="form-control" placeholder="都道府県名で検索" /></td>
+                </tr>
+                <tr>
+                    <th>県庁所在地で検索</th>
+                    <td><input type="text" name="capital" class="form-control" placeholder="県庁所在地で検索" /></td>
+                </tr>
+                <tr>
+                    <th>説明で検索</th>
+                    <td><input type="text" name="description" class="form-control" placeholder="説明で検索" /></td>
+                </tr>
+                <tr>
+                    <th>人口で検索 (最低)</th>
+                    <td>
+                        <input type="range" name="population" class="form-range" min="0" max="10000000" step="100000" value="0" oninput="document.getElementById('search-population-min').textContent = this.value;" />
+                        <span id="search-population-min">0</span>
+                    </td>
+                </tr>
+                <tr>
+                    <th>人口で検索 (最高)</th>
+                    <td>
+                        <input type="range" name="population" class="form-range" min="0" max="10000000" step="100000" value="10000000" oninput="document.getElementById('search-population-max').textContent = this.value;" />
+                        <span id="search-population-max">10000000</span>
+                    </td>
+                </tr>
+                <tr>
+                    <th>面積で検索 (最低)</th>
+                    <td>
+                        <input type="range" name="area" class="form-range" min="0" max="10000000" step="100000" value="0" oninput="document.getElementById('search-area-min').textContent = this.value;" />
+                        <span id="search-area-min">0</span>
+                    </td>
+                </tr>
+                <tr>
+                    <th>面積で検索 (最高)</th>
+                    <td>
+                        <input type="range" name="area" class="form-range" min="0" max="10000000" step="100000" value="10000000" oninput="document.getElementById('search-area-max').textContent = this.value;" />
+                        <span id="search-area-max">10000000</span>
+                    </td>
+                </tr>
+                <tr>
+                    <th>人口密度で検索 (最低)</th>
+                    <td>
+                        <input type="range" name="population_density" class="form-range" min="0" max="10000000" step="100000" value="0" oninput="document.getElementById('search-population-density-min').textContent = this.value;" />
+                        <span id="search-population-density-min">0</span>
+                    </td>
+                </tr>
+                <tr>
+                    <th>人口密度で検索 (最高)</th>
+                    <td>
+                        <input type="range" name="population_density" class="form-range" min="0" max="10000000" step="100000" value="10000000" oninput="document.getElementById('search-population-density-max').textContent = this.value;" />
+                        <span id="search-population-density-max">10000000</span>
+                    </td>
+                </tr>
+                <tr>
+                    <th>訪問済み？</th>
+                    <td>
+                        <input type="hidden" name="visited" value="0" />
+                        <input type="checkbox" name="visited" value="1" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <button type="submit" class="btn btn-outline-secondary">検索</button>
+    </form>
 </div>
